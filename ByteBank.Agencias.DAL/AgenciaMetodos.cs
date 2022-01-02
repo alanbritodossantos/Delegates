f8dfc8ad4be18cdd88f386d479b7ea5778a41263
Alan <alanbritodossantos2015@gmail.com>
2022-01-02 02:30:19 -0300

    inicio

---
+++ ByteBank.Agencias.DAL/AgenciaMetodos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Agencias.DAL
+{
+    //classe parcial pode continuar sua definição em varios arquivos diferentes
+    public partial class Agencia
+    {
+        
+        public override string ToString() =>
+            $"{Numero} - {Nome}".Trim();
+    }
+}
