b95cee9eb5f9b1017b5a6f234aa605d05b374002
Alan <alanbritodossantos2015@gmail.com>
2022-01-02 22:06:50 -0300

    exemplo sobre delegate

---
+++ ByteBank.Agencias/ExemploDeDelegates.cs
@@ -0,0 +1,64 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ByteBank.Agencias
+//{
+//    class ExemploDeDelegates 
+//    {
+//        //Definição do delegate
+//        public delegate string ObterMensagemEspecifica();
+
+//        static void Main1(String[] args)
+//        {
+//            Mensagem1();
+//            Mensagem2();
+//            Mensagem3();
+//        }
+
+//        static string MinhaMensagem1()
+//        {
+//            return "Mensagem1";
+//        }
+
+//        static void Mensagem1()
+//        {
+//            // metodo local funtion
+//            //string MinhaMensagem() => "Mensagem 1";
+
+//            //var mensagemCompleta = ObterMensagem(MinhaMensagem);
+//            var mensagemCompleta = ObterMensagem(MinhaMensagem1);
+//            Console.WriteLine(mensagemCompleta);
+//        }
+
+//        static void Mensagem2()
+//        {
+//            string MinhaMensagem() => "Mensagem 2";
+
+//            var mensagemCompleta = ObterMensagem(MinhaMensagem);
+//            Console.WriteLine(mensagemCompleta);
+//        }
+
+//        static void Mensagem3()
+//        {
+//            string MinhaMensagem() => "Mensagem 3";
+
+//            var mensagemCompleta = ObterMensagem(MinhaMensagem);
+//            Console.WriteLine(mensagemCompleta);
+//        }
+
+//        static string ObterMensagem(ObterMensagemEspecifica mensagemEspecifica)
+//        {
+//            string mensagem = string.Empty;
+//            mensagem += "Início da mensagem \r\n\r\n";
+//            mensagem += mensagemEspecifica();//delegate -- executa o metodo
+//            mensagem += "\r\n\r\n";
+//            mensagem += "Final da mensagem\r\n";
+//            mensagem += "----------------\r\n";
+
+//            return mensagem;
+//        }
+//    }
+//}
