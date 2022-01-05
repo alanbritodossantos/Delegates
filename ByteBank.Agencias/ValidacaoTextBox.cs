0b444e6be94ac3a8c37c7ee094fa0e38663d5877
Alan <alanbritodossantos2015@gmail.com>
2022-01-05 00:00:01 -0300

    Executando nossos eventos

--- ByteBank.Agencias/ValidacaoTextBox.cs
+++ ByteBank.Agencias/ValidacaoTextBox.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ByteBank.Agencias
 {
@@ -12,6 +13,21 @@ namespace ByteBank.Agencias
 
     public class ValidacaoTextBox : TextBox
     {
+        //delegate+evento
         public event ValidacaoEventHandler Validacao;
+
+        public ValidacaoTextBox()
+        {
+            TextChanged += ValidacaoTextBox_TextChanged;
+        }
+
+        private void ValidacaoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Validacao != null)
+            {
+                var ehValido = Validacao(Text);
+                Background = ehValido ? new SolidColorBrush(Colors.White) : new SolidColorBrush(Colors.OrangeRed);
+            }
+        }
     }
 }
